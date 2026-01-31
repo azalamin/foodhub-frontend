957fc35136f0eb01077e0974cc02716c1f6ffd0e
Al Amin Sheikh <azalamin@MacBookPro.netis.cc>
2026-01-31 10:12:55 +0600

    feat(navbar) mobile and desktop responisve

--- src/app/(commonLayout)/layout.tsx
+++ src/app/(commonLayout)/layout.tsx
@@ -1,3 +1,10 @@
+import { Navbar } from "@/components/layout/Navbar";
+
 export default function CommonLayout({ children }: { children: React.ReactNode }) {
-	return <div>{children}</div>;
+	return (
+		<div className='mx-auto'>
+			<Navbar />
+			{children}
+		</div>
+	);
 }
