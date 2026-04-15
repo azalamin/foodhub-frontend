c154ad9a89482c7c8fda64d14e070476f38617e3
Al Amin Sheikh <azalamin@MacBookPro.netis.cc>
2026-04-16 02:36:30 +0600

    fix: signin with google

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-	baseURL: "https://food-hub-server-lime.vercel.app",
+	baseURL: process.env.NEXT_PUBLIC_AUTH_URL || "https://food-hub-server-lime.vercel.app",
 	fetchOptions: {
 		credentials: "include",
 	},
